9dccb212e437749349b99956a867884d4bf0c1dd
hyowlion <hyowlion@gmail.com>
2026-04-13 21:30:49 +0800

    chore: 更新版本号并修复翻译文件中的引号格式

--- zhiz_print/__init__.py
+++ zhiz_print/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2026, Zhiz Print and contributors
 # For license information, please see license.txt
 
-__version__ = '15.01.07'
+__version__ = '15.01.08'
 
 required_apps = ["frappe"]
