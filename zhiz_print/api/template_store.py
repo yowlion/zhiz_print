7fa466c05def4d18c35458b6d019024a095a14d9
yowlion <hyowlion@gmail.com>
2026-07-03 17:36:17 +0800

    [v15.20.43] 脱敏正则修复:字符类加 A-Za-z 支持含英文的公司名(上海XX公司/ABC科技公司),负向先行也加 A-Za-z

--- zhiz_print/api/template_store.py
+++ zhiz_print/api/template_store.py
@@ -76,7 +76,7 @@ def share_template(design_name):
             preview_html_design = preview_html_design.replace(_c, SENSITIVE_COMPANY)
     # 兜底:正则匹配硬编码公司名(中文2+字+有限公司/科技有限公司/公司等后缀,后非中文避免"公司名称/代号"标签误匹配)
     import re
-    _company_re = re.compile(r'[一-龥]{2,15}(?:有限公司|科技有限公司|有限责任公司|股份有限公司|集团有限公司|公司)(?![一-龥])')
+    _company_re = re.compile(r'[一-龥A-Za-z]{2,15}(?:有限公司|科技有限公司|有限责任公司|股份有限公司|集团有限公司|公司)(?![一-龥A-Za-z])')
     preview_html = _company_re.sub(SENSITIVE_COMPANY, preview_html)
     preview_html_design = _company_re.sub(SENSITIVE_COMPANY, preview_html_design)
 
