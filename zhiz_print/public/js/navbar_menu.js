5cffdfd3f65b85b3f3b564377317e27fa52c947b
yowlion <hyowlion@gmail.com>
2026-07-05 23:23:28 +0800

    [v15.20.75] 修头像下拉菜单注入:用#toolbar-user精准定位+dropdown-item样式(替代find/contains,匹配Frappe v15实际DOM)

--- zhiz_print/public/js/navbar_menu.js
+++ zhiz_print/public/js/navbar_menu.js
@@ -1,10 +1,7 @@
-// 用户头像下拉菜单注入「打印模板平台」入口
-$(document).on('shown.bs.dropdown', function(e) {
-    var $menu = $(e.target).parent().find('.dropdown-menu');
-    if (!$menu.length) return;
-    // 确认是用户菜单(含 My Settings/Logout/退出)
-    if (!$menu.find('a:contains("My Settings"), a:contains("Logout"), a:contains("退出"), a:contains("设置")').length) return;
-    if ($menu.find('.zhiz-print-store-link').length) return;
-    $menu.append('<li class="divider zhiz-print-store-divider"></li>');
-    $menu.append('<li class="zhiz-print-store-link"><a href="/app/print-template-store"><i class="fa fa-fw fa-print"></i> ' + __('打印模板平台') + '</a></li>');
+// 用户头像下拉菜单注入「打印模板平台」入口(Frappe v15 #toolbar-user)
+$(document).on('shown.bs.dropdown', function() {
+    var $menu = $('#toolbar-user');
+    if (!$menu.length || $menu.find('.zhiz-print-store-link').length) return;
+    $menu.append('<div class="dropdown-divider"></div>');
+    $menu.append('<a class="dropdown-item zhiz-print-store-link" href="/app/print-template-store"><i class="fa fa-fw fa-print"></i> ' + __('打印模板平台') + '</a>');
 });
