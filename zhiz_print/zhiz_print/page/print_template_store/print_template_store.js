05be993fcaca4aa734af22d07bbfa668a596e947
yowlion <hyowlion@gmail.com>
2026-08-29 21:46:19 +0800

    [v15.21.97] 模板平台视角过滤标签:分享给我的改为指定分享的

--- zhiz_print/zhiz_print/page/print_template_store/print_template_store.js
+++ zhiz_print/zhiz_print/page/print_template_store/print_template_store.js
@@ -124,7 +124,7 @@ frappe.pages['print-template-store'].on_page_load = function (wrapper) {
             <div id="pts-filters" class="pts-filters">
                 <span class="pts-chip active" data-filter="all">${__('所有可见')} <span class="pts-chip-count" id="pts-fc-all">0</span></span>
                 <span class="pts-chip pts-chip-mine" data-filter="mine">${__('我的模板')} <span class="pts-chip-count" id="pts-fc-mine">0</span></span>
-                <span class="pts-chip pts-chip-shared" data-filter="shared">${__('分享给我的')} <span class="pts-chip-count" id="pts-fc-shared">0</span></span>
+                <span class="pts-chip pts-chip-shared" data-filter="shared">${__('指定分享的')} <span class="pts-chip-count" id="pts-fc-shared">0</span></span>
                 <span class="pts-chip pts-chip-public" data-filter="public">${__('公开共享')} <span class="pts-chip-count" id="pts-fc-public">0</span></span>
             </div>
         </div>
